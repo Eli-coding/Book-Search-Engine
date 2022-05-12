f5652e4f6c8d4ebab1784b30dd1f33a7ae67c287
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-05-12 10:45:09 -0400

    server side fix

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -5,8 +5,8 @@ type Query{
     me: User
 }
 type User{
-    _id: ID
-    username: String
+    _id: ID!
+    username: String!
     email:String
     bookcount: Int
     savedBooks: [Book]
@@ -15,21 +15,21 @@ type Book {
     bookId: id
     authors: []
     description:String
-    title: String
-    image: 
-    link:
+    title: String!
+    image: String
+    link: String
 
 }
 type Auth {
-    token:
-    user:
+    token: ID!
+    user: User
 }
 
 type Mutation {
     login (email: String!, password: String!):Auth
     addUser(username: username!, email: String!, password: String!):Auth
     saveBook(author[]: String!, description:String!,title:String!,bookId:id, image: String!, link:String! ):User
-    removeBook(bookId:id!):User
+    removeBook(bookId:ID!):User
 }
 
 `;
