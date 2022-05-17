1d25493fc530089668c458a80070fd2eb6aa9241
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-05-17 10:39:41 -0400

    pages edit

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,26 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import {LOGIN_USER} from "../utils/auth";
 
-import { loginUser } from '../utils/API';
+//import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [ login, {err}] = useMutation(LOGIN_USER);
+
+  useEffect(() =>{
+    if (err){
+      setShowAlert(true);
+    } else{
+      setShowAlert(false);
+    }
+  }, [err]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,18 +38,18 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const{data} = await login ({
+        variables:{...userFormData},
+      });
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      console.log(data);
+      Auth.login(data.login.token);
+    
+  
+    } catch (error) {
+      console.error(error);
+      //setShowAlert(true);
     }
 
     setUserFormData({
