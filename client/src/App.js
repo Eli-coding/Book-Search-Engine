1ac1b49482959a07ba9abc6ed17f341230893f2f
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-06-27 02:22:29 -0400

    catch error

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider,createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider,createHttpLink } from '@apollo/client';
+
 import {setContext} from '@apollo/client/link/context';
 
 import SearchBooks from './pages/SearchBooks';
